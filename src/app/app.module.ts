e616b807ccdeb88e7c5c0b20128f1fa4b376a8f4
KevinSchimke <schimke.94@gmx.de>
2023-01-06 02:04:41 +0100

    login card, check if input field valid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,7 +30,10 @@ import { DashboardComponent } from './components/main/dashboard/dashboard.compon
 
 // Ng Material
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -52,8 +56,12 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     provideFirestore(() => getFirestore()),
     provideAuth(() => getAuth()),
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     MatCardModule,
-    MatSidenavModule
+    MatFormFieldModule,
+    MatSidenavModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/authentication/login/login.component.ts
+++ src/app/components/authentication/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./../authentication.component.scss']
 })
 export class LoginComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.minLength(6)]);
+  hide = true;
 
+  getErrorMessage(formControl: string) {
+    if (formControl == 'email') {
+      if (this.email.hasError('required')) return 'You must enter a value';
+      if (this.email.hasError('email')) return 'Not a valid email';
+    }
+    if (formControl == 'password') {
+      if (this.password.hasError('required')) return 'You must enter a password';
+      if (!this.password.valid) return 'Your password is short';
+    }
+    return '';
+  }
 }
