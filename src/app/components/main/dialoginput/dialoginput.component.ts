9a8ae65140a1a05f55b558544129813f36c399d6
Valer Neufeld <valerneufeld@outlook.de>
2023-01-06 16:52:23 +0100

    textarea V0.2

--- src/app/components/main/dialoginput/dialoginput.component.ts
+++ src/app/components/main/dialoginput/dialoginput.component.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/models/user.class';
+import { Comment } from 'src/app/models/comment.class';
 
 @Component({
   selector: 'app-dialoginput',
   templateUrl: './dialoginput.component.html',
-  styleUrls: ['./dialoginput.component.scss']
+  styleUrls: ['./dialoginput.component.scss'],
 })
 export class DialoginputComponent {
+  user: User = Object();
 
   message: string = '';
   chat: string[] = [];
+  comments: any[] = [];
 
   getMessage() {
+    let comment: Comment = Object();
+
+    this.user.id = 'testuser';
+    comment.userid = this.user.id + this.comments.length;
+    comment.message = this.message;
+    this.comments.push(comment);
     this.chat.push(this.message);
     this.message = '';
+    console.log(this.comments);
   }
-
 }
