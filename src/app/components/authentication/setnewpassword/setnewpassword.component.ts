ced1977e1d48cf7157a03b9ea4619dd19db2ab19
KevinSchimke <schimke.94@gmx.de>
2023-01-06 22:03:40 +0100

    set new  password component

--- src/app/components/authentication/setnewpassword/setnewpassword.component.ts
+++ src/app/components/authentication/setnewpassword/setnewpassword.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-setnewpassword',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./../authentication.component.scss']
 })
 export class SetnewpasswordComponent {
+  password = new FormControl('', [Validators.required, Validators.minLength(6)]);
+  hide = true;
 
+  getErrorMessage(formControl: string) {
+    if (formControl == 'password') {
+      if (this.password.hasError('required')) return 'You must enter a password';
+      if (!this.password.valid) return 'Your password is short';
+    }
+    return '';
+  }
 }
