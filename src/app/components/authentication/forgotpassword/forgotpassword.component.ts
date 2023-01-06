8a68be3de28ae47362a48443c415690d0f91eddf
KevinSchimke <schimke.94@gmx.de>
2023-01-06 21:52:28 +0100

    forgot password component

--- src/app/components/authentication/forgotpassword/forgotpassword.component.ts
+++ src/app/components/authentication/forgotpassword/forgotpassword.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgotpassword',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./../authentication.component.scss']
 })
 export class ForgotpasswordComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
 
+  getErrorMessage(formControl: string) {
+    if (formControl == 'email') {
+      if (this.email.hasError('required')) return 'You must enter a value';
+      if (this.email.hasError('email')) return 'Not a valid email';
+    }
+    return '';
+  }
 }
