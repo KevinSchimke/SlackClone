e3a91d0d69f15550628ec2b62f857bbe867759d4
RaphaelKono <raphaelkonopatzki@web.de>
2023-01-07 19:20:44 +0100

    formatting of directive

--- src/app/directives/click-stop-propagation/click-stop-propagation.directive.ts
+++ src/app/directives/click-stop-propagation/click-stop-propagation.directive.ts
@@ -6,6 +6,7 @@ import { Directive, HostListener } from '@angular/core';
 export class ClickStopPropagationDirective {
 
   constructor() { }
+  
   @HostListener("click", ["$event"])
   public onClick(event: any): void
   {
