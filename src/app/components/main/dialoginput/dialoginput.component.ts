64940020ce28a0d1acc1e8b4371cc68b5bdcd0c9
Valer Neufeld <valerneufeld@outlook.de>
2023-01-07 09:41:07 +0100

    emoij picker

--- src/app/components/main/dialoginput/dialoginput.component.ts
+++ src/app/components/main/dialoginput/dialoginput.component.ts
@@ -1,6 +1,28 @@
 import { Component } from '@angular/core';
 import { User } from 'src/app/models/user.class';
 import { Comment } from 'src/app/models/comment.class';
+import { EmojiEvent } from '@ctrl/ngx-emoji-mart/ngx-emoji';
+
+const CUSTOM_EMOJIS = [
+  {
+    name: 'Party Parrot',
+    shortNames: ['parrot'],
+    keywords: ['party'],
+    imageUrl: './assets/images/parrot.gif',
+  },
+  {
+    name: 'Octocat',
+    shortNames: ['octocat'],
+    keywords: ['github'],
+    imageUrl: 'https://github.githubassets.com/images/icons/emoji/octocat.png',
+  },
+  {
+    name: 'Squirrel',
+    shortNames: ['shipit', 'squirrel'],
+    keywords: ['github'],
+    imageUrl: 'https://github.githubassets.com/images/icons/emoji/shipit.png',
+  },
+];
 
 @Component({
   selector: 'app-dialoginput',
@@ -9,7 +31,6 @@ import { Comment } from 'src/app/models/comment.class';
 })
 export class DialoginputComponent {
   user: User = Object();
-
   message: string = '';
   chat: string[] = [];
   comments: any[] = [];
@@ -24,5 +45,51 @@ export class DialoginputComponent {
     this.chat.push(this.message);
     this.message = '';
     console.log(this.comments);
+  }
+
+  themes = [
+    'native',
+    'apple',
+    'google',
+    'twitter',
+    'facebook',
+  ];
+
+  darkMode: undefined | boolean = !!(
+    typeof matchMedia === 'function' &&
+    matchMedia('(prefers-color-scheme: dark)').matches
+  );
+
+  darkestMode: undefined | boolean = undefined;
+  set = 'native';
+  native = true;
+  CUSTOM_EMOJIS = CUSTOM_EMOJIS;
+
+  setTheme(set: string) {
+    this.native = set === 'native';
+    this.set = set;
+  }
+  setDarkmode(mode: boolean | undefined) {
+    if (mode === undefined) {
+      this.darkestMode = mode;
+      this.darkMode = !!(
+        typeof matchMedia === 'function' &&
+        matchMedia('(prefers-color-scheme: dark)').matches
+      );
+    } else {
+      this.darkMode = mode;
+      this.darkestMode = mode;
+    }
+  }
+  handleClick($event: EmojiEvent) {
+    console.log($event.emoji);
+    this.message += $event.emoji.native;
+  }
+  emojiFilter(e: string): boolean {
+    // Can use this to test [emojisToShowFilter]
+    if (e && e.indexOf && e.indexOf('1F4') >= 0) {
+      return true;
+    }
+    return false;
   }
 }
