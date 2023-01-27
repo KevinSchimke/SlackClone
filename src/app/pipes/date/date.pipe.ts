b3e0bac5b69a8df3b0d5513cf428e2812bbd1e5a
KevinSchimke <schimke.94@gmx.de>
2023-01-27 13:27:41 +0100

    info usercard error date

--- src/app/pipes/date/date.pipe.ts
+++ src/app/pipes/date/date.pipe.ts
@@ -6,7 +6,7 @@ import { Timestamp } from '@angular/fire/firestore';
 })
 export class DatePipe implements PipeTransform {
   transform(value: Timestamp | Date | string): string {
-    if((typeof value !== 'string')){
+    if ((typeof value !== 'string')) {
       if (!(value instanceof Date)) value = value.toDate();
       if (value instanceof Date) value = value.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
     }
