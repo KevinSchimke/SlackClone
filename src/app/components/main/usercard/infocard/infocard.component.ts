d53febf2df19c401280141b105d8731995e7c3d6
KevinSchimke <schimke.94@gmx.de>
2023-01-28 18:36:07 +0100

    childsector

--- src/app/components/main/usercard/infocard/infocard.component.ts
+++ src/app/components/main/usercard/infocard/infocard.component.ts
@@ -19,11 +19,19 @@ export class InfocardComponent {
   userActive?: boolean;
   lastLogin: any
 
-  constructor(public sidenavToggler: SidenavToggleService, private router: Router, public userService: UserService, private firestoreService: FirestoreService, private dialog: MatDialog, private route: ActivatedRoute) { }
+  constructor(public childSelector: SidenavToggleService, private router: Router, public userService: UserService, private firestoreService: FirestoreService, private dialog: MatDialog, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((params: any) => {
       this.getUser(params.id);
+    });
+    this.subscribeThreadbarInit();
+  }
+
+  subscribeThreadbarInit() {
+    this.childSelector.threadBarIsInit.subscribe(isLoaded => {
+      if (isLoaded === true)
+        this.childSelector.threadBar.open();
     });
   }
 
@@ -31,7 +39,6 @@ export class InfocardComponent {
     this.user$ = this.firestoreService.getUser(uid);
     this.user$.subscribe((user: User) => {
       this.user = user;
-      // this.userActive = false;
       this.userState();
     });
   }
@@ -39,39 +46,20 @@ export class InfocardComponent {
   userState() {
     let lastLogin = this.user.lastLogin.toDate().getTime();
     let currentTime = new Date().getTime();
-    console.log(lastLogin);
-    console.log(currentTime);
-    console.log(currentTime - lastLogin > 6000);
 
-
-    if (currentTime - lastLogin < 60000) {
+    if (currentTime - lastLogin < 300000) {
       this.userActive = true;
     } else {
       this.userActive = false;
     }
   }
 
-  // userState() {
-  //   let lastLoginTimestamp = this.user.lastLogin;
-  //   let lastLoginTimestamp2 = this.user.lastLogin.toDate();
-  //   let lastLoginDate = new Date((lastLoginTimestamp.seconds + lastLoginTimestamp.nanoseconds / 1e9) * 1000);
-  //   let currentTime = new Date().getTime();
-
-  //   if (currentTime - lastLoginDate.getTime() > 6000) {
-  //     this.userActive = true;
-  //   } else {
-  //     this.userActive = false;
-  //   }
-  // }
-
-
-
   openDialog(): void {
     this.dialog.open(ReauthenticateComponent);
   }
 
   closeThread() {
-    this.sidenavToggler.threadBar.close();
+    this.childSelector.threadBar.close();
     this.router.navigate([{ outlets: { right: null } }], { relativeTo: this.route.parent });
   }
 }
