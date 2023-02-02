9c22c57827fa9c035973552507bdeaf965fbe66d
RaphaelKono <github-20230129-1818@konopatzki.info>
2023-02-02 14:02:46 +0100

    started combined query of all channels

--- src/app/components/main/dashboard/mat-drawer-content/all-channels/all-channels.component.ts
+++ src/app/components/main/dashboard/mat-drawer-content/all-channels/all-channels.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { collection, Firestore, getDocs, query, where } from '@angular/fire/firestore';
+import { SidenavToggleService } from 'src/app/service/sidenav-toggle/sidenav-toggle.service';
+import { UserService } from 'src/app/service/user/user.service';
+import { FirestoreService } from 'src/app/service/firebase/firestore.service';
 
 @Component({
   selector: 'app-all-channels',
@@ -6,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./all-channels.component.scss']
 })
 export class AllChannelsComponent {
+  leftSideBar: boolean = false;
 
+  constructor(public sidenavToggler: SidenavToggleService, private firestore: Firestore, private userService: UserService, private firestoreService: FirestoreService) { }
+
+  async ngOnInit() {
+    let channelsRef = collection(this.firestore, 'channels');
+    const openChannelsQuery = query(channelsRef, where("locked", "==", false));
+    const joinedChannelsQuery = this.firestoreService.getCurrentUserData('channels', 'users', this.userService.getUid());
+    const [isOpenChannels, isJoinedChannels] = await Promise.all([
+      await getDocs(openChannelsQuery),
+      await getDocs(joinedChannelsQuery)
+    ]);
+    console.log('openChannels are ', isOpenChannels);
+    // const isOpenChannelsArray = isOpenChannels.doc;
+    //     const isJoinedChannelsArray = isJoinedChannels.docs;
+
+    // const channelsArray = isOpenChannelsArray.concat(isJoinedChannelsArray);
+  }
+
+
+  toggleLeftSidebar() {
+    this.leftSideBar = !this.leftSideBar;
+    this.sidenavToggler.workspaceBar.toggle()
+  }
 }
