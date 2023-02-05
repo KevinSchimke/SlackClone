8f4767bdd7dd75fd0fd50984c7232e4ab71cc648
KevinSchimke <schimke.94@gmx.de>
2023-02-05 21:45:25 +0100

    sidenav start

--- src/app/components/main/dashboard/dashboard.component.ts
+++ src/app/components/main/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
-import { Component, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { MatDrawerMode } from '@angular/material/sidenav';
 import { FirestoreService } from 'src/app/service/firebase/firestore/firestore.service';
 import { SidenavToggleService } from 'src/app/service/sidenav-toggle/sidenav-toggle.service';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,14 +10,27 @@ import { SidenavToggleService } from 'src/app/service/sidenav-toggle/sidenav-tog
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  mobileQuery: MediaQueryList;
 
   @ViewChild('workspaceBar') workspaceBar: any;
   @ViewChild('threadBar') threadBar: any;
 
-  constructor(public createChannelService: FirestoreService, public sidenavToggler: SidenavToggleService) { }
+  private _mobileQueryListener: () => void;
+
+  constructor(public createChannelService: FirestoreService, public sidenavToggler: SidenavToggleService, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 1000px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
 
   ngAfterViewInit() {
     this.sidenavToggler.setWorkspaceBar(this.workspaceBar);
     setTimeout(() => this.sidenavToggler.setThreadBar(this.threadBar), 0);
+  }
+
+  toggleMode() {
+    let side: MatDrawerMode = 'side'
+    let over: MatDrawerMode = 'over'
+    return (window.innerWidth >= 1000) ? side : over
   }
 }
