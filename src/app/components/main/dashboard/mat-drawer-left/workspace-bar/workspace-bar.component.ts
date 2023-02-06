1da2ea45989fc70e0b9b75667796d25a50e9e5e1
RaphaelKono <github-20230129-1818@konopatzki.info>
2023-02-06 15:45:40 +0100

    reestablished correct routing in workspace-bar.component

--- src/app/components/main/dashboard/mat-drawer-left/workspace-bar/workspace-bar.component.ts
+++ src/app/components/main/dashboard/mat-drawer-left/workspace-bar/workspace-bar.component.ts
@@ -121,7 +121,6 @@ export class WorkspaceBarComponent {
   }
 
   navigateToRoute(url: string) {
-    // this.router.navigateByUrl(url);
     this.router.navigate( [url] , { relativeTo: this.route });
     this.toggleSidenav();
   }
